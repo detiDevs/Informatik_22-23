a1c000585507fcc05217672ca394bfb89f52b6fc
detiDevs <lucasteinhausen@web.de>
2022-03-18 10:39:25 +0000

    fibonacci zeugs

---
+++ Fibonacci.java
@@ -0,0 +1,28 @@
+public class Fibonacci {
+    
+     public Fibonacci(){
+
+     }
+
+    public static long fibRekursiv(int n){
+
+        if(n==1 || n==2){
+            return 1;
+        }
+        else{
+            return fibRekursiv(n-1) + fibRekursiv(n-2);
+        }
+    }
+
+    public static long fibIterativ(int n){
+        int fib = 0;
+        int h1 = 0;
+        int h2 = 1;
+        for(int i = 0; i<=n; i++){
+            fib = h1+h2;
+            h2 = h1;
+            h1 = fib;
+        }
+       return fib;
+    }
+}
