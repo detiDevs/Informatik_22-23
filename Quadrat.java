495946e5aceae7cee6924827a46dfc38ccbe46d3
detiDevs <lucasteinhausen@web.de>
2022-03-25 10:32:04 +0000

    iterative Quadratzahl funktioniert jetzt!

--- Quadrat.java
+++ Quadrat.java
@@ -1,13 +1,9 @@
-import java.util.LinkedList;
-
-import java.util.ArrayList;
-
 public class Quadrat{
     public Quadrat(){}
-
-    public int quadIterativ(int n){
+    
+    public static int quadIterativ(int n){
         int quad = 0;
-        for(int i=1; i<n; i++){
+        for(int i=1; i<=n; i++){
             quad += (i*2 - 1);
         }
         return quad;
