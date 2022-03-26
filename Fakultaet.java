3b6b1564b5250237e390b46af5e0b1d29ae5c435
detiDevs <lucasteinhausen@web.de>
2022-03-26 23:22:50 +0100

    jetzt  funzt alles

--- Fakultaet.java
+++ Fakultaet.java
@@ -18,6 +18,6 @@ public class Fakultaet {
         for(int i=1; i<=n; i++){
             fak *= i;
         }
-        return n;
+        return fak;
     }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Fakultaet.fakRekursiv(5));
+        System.out.println(Fakultaet.fakIterativ(5));
     }
 }
