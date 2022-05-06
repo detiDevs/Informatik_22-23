5fb1337281875ba429789c7a268128a326c812ba
detiDevs <lucasteinhausen@web.de>
2022-05-06 10:05:00 +0000

    trees added

---
+++ Trees/BinaryTree.java
@@ -0,0 +1,17 @@
+package Trees;
+
+public class BinaryTree{
+    String name = "";
+    BinaryTree left = null;
+    BinaryTree right = null;
+
+    public BinaryTree(String self, BinaryTree left, BinaryTree right){
+        this.name = self;
+        this.left = left;
+        this.right = right;
+    }
+
+    public void besuche(){
+        
+    }
+}
